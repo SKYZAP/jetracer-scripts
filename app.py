f5c091ec4f0f039db4abbbfb2f80d5531403a773
Skyzap <izzat.jehan@gmail.com>
2021-10-08 01:15:52 +0800

    Deleted app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template, Response
+from aiohttp import web
+import aiohttp
+import json
+import asyncio
+import websockets
+import numpy as np
+from flask import Flask, render_template, Response, Blueprint
+from flask_sockets import Sockets
 import cv2
+import multiprocessing
 
 app = Flask(__name__)
 
-gst_str="nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12 ! nvvidconv ! video/x-raw, width=(int)1280, height=(int)720, format=(string)BGRx ! videoconvert ! appsink"
+gst_str = "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12 ! nvvidconv ! video/x-raw, width=(int)1280, height=(int)720, format=(string)BGRx ! videoconvert ! appsink"
 
 camera = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
 
 def gen_frames():  # generate frame by frame from camera
     while True:
@@ -22,7 +31,7 @@ def gen_frames():  # generate frame by frame from camera
 
 @app.route('/video_feed')
 def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
+    # Video streaming route. Put this in the src attribute of an img tag
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
@@ -32,5 +41,28 @@ def index():
     return render_template('index.html')
 
 
+def startFlaskApp():
+    app.run(host='0.0.0.0', ssl_context='adhoc', threaded=True,
+            port="5000")
+
+
+async def wshandler(websocket, path):
+    while True:
+        data = [{"message": np.random.randint(0, 1000)}]
+        await websocket.send(json.dumps(data))
+        await asyncio.sleep(1)
+
+
+def startWSServer():
+    start_server = websockets.serve(wshandler, port=5555)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',ssl_context='adhoc',port="5000",threaded=True,debug=True)
+    flaskServ = multiprocessing.Process(target=startFlaskApp)
+    asyncioServ = multiprocessing.Process(target=startWSServer)
+    flaskServ.start()
+    asyncioServ.start()
+    flaskServ.join()
+    asyncioServ.join()
